6bc348c9614387b2616ff47267cd1474647e6ee3
Isaac2036 <isaacpablo0596@hotmail.com>
2023-10-24 22:44:19 -0500

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -26,6 +26,7 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        storage.new(self)
     def __str__(self):
         """
         Este metodo regresa una representación de nuestra instancia
