832e4bab8d00940b3a8a2c32f9a9fdb801e16e8a
Jhony <Jhonny14_1@hotmail.com>
2023-10-24 22:46:40 -0500

    Se actualiza base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -25,7 +25,7 @@ class BaseModel:
         """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now().isoformat()
-        self.updated_at = self.created_at
+        self.updated_at = datetime.datetime.now().isoformat()
 
     def __str__(self):
         """
