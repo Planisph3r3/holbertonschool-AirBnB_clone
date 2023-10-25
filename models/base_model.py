ad5ebf2362376f20082012242df5bff852274934
Isaac2036 <isaacpablo0596@hotmail.com>
2023-10-25 01:56:57 -0500

    Fix: Espacio en los comentarios

--- models/base_model.py
+++ models/base_model.py
@@ -16,13 +16,8 @@ class BaseModel:
         """
         Este método inicializa los atributos:
             id, created_at, updated_at
-        Attr:
-            id : Genera un id cada vez que se instancia.
-            created_ad : Genera la hora y fecha cada vez
-                    que se instancia un nuevo objeto.
-            updated_at : Genera y actualiza la hora y fecha,
-                    cada vez que se cambia nuestra instancia.
         """
+
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
@@ -31,18 +26,21 @@ class BaseModel:
         """
         Este metodo retorna una representación de nuestra instancia
         """
+
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """
         Este método actualiza la fecha de creación updated_at.
         """
+
         self.updated_at = datetime.now()
 
     def to_dict(self):
         """
         Este método retorna un diccionario con los atributos de instancia.
         """
+        
         my_dict = dict(self.__dict__)
         my_dict['created_at'] = my_dict['created_at'].isoformat()
         my_dict['updated_at'] = my_dict['updated_at'].isoformat()
