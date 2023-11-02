a61f315b8bf5965bdba282f27a003b1121af503c
Jhony <Jhonny14_1@hotmail.com>
2023-11-02 18:20:26 -0500

    Se actualiza test_file_storage.py

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -4,12 +4,14 @@ from time import sleep
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 
+
 class TestStorage(unittest.TestCase):
     def test_file_path(self):
         self.assertIsNone(FileStorage.__file_path)
 
     def test_update_now(self):
-        bs = BaseModel()
+        model = BaseModel()
+        original_updated_at = model.updated_at
         sleep(1)
-        bs.save()
-        self.assertIsNot(bs.created_at, bs.updated_at)
+        model.save()
+        self.assertNotEqual(original_updated_at, model.updated_at)
