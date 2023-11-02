a50520d6f9e39d60721e0d4537bf94c49a080e14
Jhony <Jhonny14_1@hotmail.com>
2023-11-02 18:16:39 -0500

    Se actualiza test_file_storage.py

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 import unittest
-
+from time import sleep
 from models.engine.file_storage import FileStorage
-
+from models.base_model import BaseModel
 
 class TestStorage(unittest.TestCase):
     def test_file_path(self):
         self.assertIsNone(FileStorage.__file_path)
+
+    def test_update_now(self):
+        bs = BaseModel()
+        sleep(1)
+        bs.save()
+        self.assertIsNot(bs.created_at, bs.updated_at)
