8cfd66f4b14aa6f10ff94b58428b7645df60e962
Jhony <Jhonny14_1@hotmail.com>
2023-11-03 12:15:07 -0500

    docs: Agrega docstring a console.py

--- console.py
+++ console.py
@@ -1,7 +1,16 @@
+#!/usr/bin/python3
+"""
+Este módulo expone la clase
+HBNBCommand(cmd.Cmd)
+"""
 import cmd
 
 
 class HBNBCommand(cmd.Cmd):
+    """
+    Esta clase hereda de cmd.Cmd, desde aqui se gestionará
+    nuestro proyecto.
+    """
     prompt = "(hbnb) "
 
     def do_quit(self, arg):
