50216ef9ac77e5bd1a5fc3186ecdbb62097d54b4
Isaac2036 <isaacpablo0596@hotmail.com>
2023-11-05 17:50:15 -0500

    feat: creacion test_bm

--- tests/test_basemodel.py
+++ tests/test_basemodel.py
@@ -1,1 +1,12 @@
+import unittest
 
+from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+
+    def setUp(self):
+        self.bm = BaseModel()
+        
+    def test_init(self):
+        self.assertIsInstance(self.bm.id, str)
